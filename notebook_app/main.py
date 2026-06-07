9efddddf8e580c65daef70e18c7eb374f4bbb772
Ghost010987 <gean.depp@gmail.com>
2026-06-07 06:23:26 +0530

    Set up main entry point for the notebook app
    Implement main function to initialize application.

--- notebook_app/main.py
+++ notebook_app/main.py
@@ -1,1 +1,23 @@
+"""This is going to be the single point of entry that boots
+    the entire application. Nothing else should start the app."""
 
+import customtkinter as ctk
+from ui.app_window import AppWindow
+
+# This main.py should do 4 things:
+# Set the appearence mode
+# Set the color theme using
+# Instantiate AppWindow and store it as app
+# Call app.mainloop() to start the UI event loop
+
+def main():
+    # Set appearence and theme
+    ctk.set_appearance_mode("dark")     #"Dark", "Light", "system"
+    ctk.set_default_color_theme("blue") #"blue", "green", "dark-blue"
+
+    # Create and run the app
+    app = AppWindow()
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
